f85c6984723f07cd2a3f8b5f303f67343084e19a
Kamlesh Bind <Kamlesh.Bind@tatacommunications.com>
2022-08-21 18:33:14 +0530

    updated setup origin and created a temp ipynb file for testing

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from typing import List
 
 #Declaring variables for setup functions
 PROJECT_NAME="housing-predictor"
-VERSION="0.0.3"
+VERSION="0.0.4"
 AUTHOR="Avnish Yadav"
 DESRCIPTION="This is a first FSDS Nov batch Machine Learning Project"
 
